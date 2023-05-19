94e739753d88a5270439df3cb83f6d259d7dfbb1
CaioSass <caio.csc@hotmail.com.br>
2023-05-18 21:44:52 -0300

    18/05/2023

---
+++ JavaScriptFormulário/formulário.js
@@ -0,0 +1,6 @@
+function MostrarNome() {
+    let Nome = document.getElementById('pNome').value;
+    let Sobrenome = document.getElementById('pSobre').value;
+    let NomeCompleto = Nome + ' ' + Sobrenome;
+    alert(' Olá '+ NomeCompleto);
+}
